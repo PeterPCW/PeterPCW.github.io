31b9275aed2bec9b321d5bd74c6129eff6e08807
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-04-27 14:37:48 -0500

    style fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -25,6 +25,7 @@ const Intro = () => {
           <h2>Dad Joker<Typing.Speed ms={33} />...</h2><Typing.Delay ms={10000000000} />
         </Typing>
       </div>
+      <div></div>
       <div className="image-container">
         <img src={hexagon} alt="That's me!" className="profile"/>
       </div>
