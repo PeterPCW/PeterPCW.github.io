b3758ead974e9f9ccb2354bb170ddae746cf4e01
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-05-01 14:25:06 -0500

    commit from deploy

--- src/API/about.tsx
+++ src/API/about.tsx
@@ -6,27 +6,27 @@ import unwImg from '../images/about-imgs/unwind-img.png'*/
 const aboutMeArr = [
   {
     title: 'Chef',
-    desc: "To unwind I enjoy cooking by the seat of my pants. /n/nAt the stove or in a mixing bowl, it's all feel without a care for measurement or timers. /n/nAlways fun, and usually tasty too!",
+    desc: "To unwind I enjoy cooking by the seat of my pants.<br><br>At the stove or in a mixing bowl, it's all feel without a care for measurement or timers.<br><br>Always fun, and usually tasty too!",
     isActive: 'is-active'
   },
   {
     title: 'Hiker',
-    desc: 'I have a young active family, and Austin is a perfect fit. /n/nWe love to get out into nature and explore new trails, as long as they lead to a park!',
+    desc: 'I have a young active family, and Austin is a perfect fit.<br><br>We love to get out into nature and explore new trails, as long as they lead to a park!',
     isActive: ''
   },
   {
     title: 'Leaner',
-    desc: "I haven't logged off of ChatGPT since November. Amazing resource, and I'm soaking up all that I can get. /n/n Otherwise I'm on Twitter to absorb lessons in coding, marketing, sales, leadership, and generally being a better human.",
+    desc: "I haven't logged off of ChatGPT since November. Amazing resource, and I'm soaking up all that I can get.<br><br>Otherwise I'm on Twitter to absorb lessons in coding, marketing, sales, leadership, and generally being a better human.",
     isActive: ''
   },
   {
     title: 'Advocate',
-    desc: "Having a child with Down Syndrome has taught me to fight for inclusive environments and to go out of my way with empathy and vulnerability.",
+    desc: "Having a child with Down Syndrome has taught me to fight for inclusive environments and to go out of my way with empathy and vulnerability.<br><br>This is immensely helpful when working with difficult customers. :)",
     isActive: ''
   },
   {
     title: 'Joker',
-    desc: "I may be shy at first, but I will quickly begin to show a wealth of sarcasm, pop culture references, and dad jokes to lighten any mood. /n/nDid you know diarrhea is hereditary?",
+    desc: "I may be shy at first, but I will quickly begin to show a wealth of sarcasm, pop culture references, and dad jokes to lighten any mood.<br><br>Did you know diarrhea is hereditary?",
     isActive: ''
   }
 ]
