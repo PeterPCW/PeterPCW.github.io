241fe424c7e273f435a5c479176a66596ceaa804
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-05-01 13:21:13 -0500

    move to /docs

--- src/API/about.tsx
+++ src/API/about.tsx
@@ -6,27 +6,27 @@ import unwImg from '../images/about-imgs/unwind-img.png'*/
 const aboutMeArr = [
   {
     title: 'Chef',
-    desc: "To unwind I enjoy cooking by the seat of my pants. At the stove or the mixing bowl it's all feel without a care for measurement or timers. Always fun, and usually tasty too!",
+    desc: "To unwind I enjoy cooking by the seat of my pants. /n/nAt the stove or in a mixing bowl, it's all feel without a care for measurement or timers. /n/nAlways fun, and usually tasty too!",
     isActive: 'is-active'
   },
   {
-    title: 'Ranger',
-    desc: 'I have a young active family, and Austin is a perfect fit. We love to get out into nature and explore new trails, as long as they lead to a park!',
+    title: 'Hiker',
+    desc: 'I have a young active family, and Austin is a perfect fit. /n/nWe love to get out into nature and explore new trails, as long as they lead to a park!',
     isActive: ''
   },
   {
     title: 'Leaner',
-    desc: "I haven't logged off of ChatGPT since November. Otherwise I'm on Twitter to absorb lessons in coding, marketing, sales, leadership, and generally being a better human.",
-    isActive: ''
-  },
-  {
-    title: 'Joker',
-    desc: "I may be shy at first, but I will soon begin to show a wealth of sarcasm, pop culture references, and dad jokes to lighten any mood.",
+    desc: "I haven't logged off of ChatGPT since November. Amazing resource, and I'm soaking up all that I can get. /n/n Otherwise I'm on Twitter to absorb lessons in coding, marketing, sales, leadership, and generally being a better human.",
     isActive: ''
   },
   {
     title: 'Advocate',
-    desc: "Having a child with Down Syndrome has taught me to fight for inclusive environments and focus on proactively offering empathy and vulnerability.",
+    desc: "Having a child with Down Syndrome has taught me to fight for inclusive environments and to go out of my way with empathy and vulnerability.",
+    isActive: ''
+  },
+  {
+    title: 'Joker',
+    desc: "I may be shy at first, but I will soon begin to show a wealth of sarcasm, pop culture references, and dad jokes to lighten any mood. /n/nDid you know diarrhea is hereditary?",
     isActive: ''
   }
 ]
