8c00953f32bcaf86d073412347156a72bf2ed619
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-04-30 23:20:00 -0500

    styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,26 +30,28 @@ function About() {
 
   return <section className="section" id="about">
     <section className="content">
-      <h2><span className="orange">{'//'}</span> A Little About Me</h2>
-      <div className="about-box">
-        <article className="pt-one">
-          <ul className="tabs">
-            {aboutMeArr.map((interest, i) => {
-              return <button className={`${interest.isActive} interest-tab`} value={i} key={interest.title} onClick={handleChangeInterest}>{interest.title}</button>
-            })}
-          </ul>
-        </article>
-        <article className="pt-two">
-          {/*<div className="img-cont">
-          <img src={currentInterest.image} />
-        </div>*/}
-        </article>
-        <article className="pt-three">
-          <div>
-            <h3><span className="orange">{'//'}</span> {currentInterest.title}</h3>
-            <p>{currentInterest.desc}</p>
-          </div>
-        </article>
+      <div className="about-cont">
+        <h2><span className="orange">{'//'}</span> A Little About Me</h2>
+        <div className="about-box">
+          <article className="pt-one">
+            <ul className="tabs">
+              {aboutMeArr.map((interest, i) => {
+                return <button className={`${interest.isActive} interest-tab`} value={i} key={interest.title} onClick={handleChangeInterest}>{interest.title}</button>
+              })}
+            </ul>
+          </article>
+          <article className="pt-two">
+            {/*<div className="img-cont">
+            <img src={currentInterest.image} />
+          </div>*/}
+          </article>
+          <article className="pt-three">
+            <div>
+              <h3><span className="orange">{'//'}</span> {currentInterest.title}</h3>
+              <p>{currentInterest.desc}</p>
+            </div>
+          </article>
+        </div>
       </div>
     </section>
   </section>
