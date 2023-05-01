58e100ce837d573e5652f4ab621e6bc1677c1d24
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-05-01 13:59:07 -0500

    commit from deploy

--- src/API/learning.tsx
+++ src/API/learning.tsx
@@ -1,8 +1,8 @@
 import linux from '../images/svg-icons/linux-plain.svg'
+import selenium from '../images/svg-icons/selenium.svg'
 import java from '../images/svg-icons/java-plain.svg'
 import springboot from '../images/svg-icons/springboot-plain.svg'
-import csharp from '../images/svg-icons/csharp-plain.svg'
 
-const learnArray = [{ name: 'Linux', svg: linux }, { name: 'Java', svg: java }, { name: 'SpringBoot', svg: springboot }, { name: 'C#', svg: csharp }]
+const learnArray = [{ name: 'Linux', svg: linux }, { name: 'Selenium', svg: selenium }, { name: 'Java', svg: java }, { name: 'SpringBoot', svg: springboot }]
 
 export default learnArray
