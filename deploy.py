fbc1d098d862c2babba29dde9e87f31645a0b947
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-04-30 22:17:42 -0500

    deploy updates

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 import os
 # import shutil
 # import imgkit
+import time
 
 # Run the API tests
 # os.system("coverage run -m pytest")
@@ -15,8 +16,17 @@ os.system('cd .. && cd .. && cd ..')
 # Build the React app - includes eslint and jest per package.json
 os.system("npm run lintbuild")
 
+# Ask for commit message with timeout
+print("Please enter a commit message (30s timeout):")
+try:
+    commit_msg = input()
+    time.sleep(30)  # Wait for 30 seconds
+except:
+    commit_msg = "commit from deploy"  # Timeout, set default message
+
 # Commit to deploy
-os.system('git add . && git commit -m "commit from deploy" && git push')
+os.system(f'git add . && git commit -m "{commit_msg}" && git push')
+
 
 #### Old Code #### replace with copy to prod on test pass or something
 #print('# Add gh-pages remote and deploy to gh-pages')
