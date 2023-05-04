0f4e9122473f9f3d919f1a957829ae91804a0d1c
PeterPCW <49111837+PeterPCW@users.noreply.github.com>
2023-05-04 11:16:07 -0500

    commit from deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,15 @@ import Exp from './components/Exp'
 import About from './components/About'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Spacer from './components/Spacer'
 
 function App() {
   return <main data-testid="app-component">
     <Intro />
     <Nav />
-    <Spacer />
     <Projects />
     <Skills />
     <Exp />
     <About />
-    <Spacer />
     <Contact />
     <Footer />
   </main>
